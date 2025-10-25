a8750d09ce893dc6caac5cc4ef8c3e241b84cb84
jaimaheshwari1706 <jaimaheshwari943@gmail.com>
2025-10-25 11:55:01 +0530

    Initial commit: Angular + Tailwind portfolio

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,28 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
+import { Hero } from './components/hero/hero';
+import { About } from './components/about/about';
+import { Projects } from './components/projects/projects';
+import { Skills } from './components/skills/skills';
+import { Contact } from './components/contact/contact';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    Navbar,
+    Hero,
+    About,
+    Projects,
+    Skills,
+    Contact,
+    Footer
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
   protected readonly title = signal('portfolio-app');
 }
+
